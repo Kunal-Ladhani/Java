4754d8910667def724004d73af95282dba9d8d40
Kunal Ladhani <k.ladhani1@gmail.com>
2024-02-19 00:32:34 +0530

    first commit

---
+++ Scanner_Template.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Scanner_Template {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while (t-- > 0) {
+
+        }
+        scn.close();
+    }
+}
